e2a3f3e80cbdc4c692211dd66df153065826d9d4
Archna Ashish <ashisharchna@gmail.com>
2020-04-21 23:28:45 -0400

    created the sql files and loaded csv into sql tables

---
+++ EmployeeSQL/employee.sql
@@ -0,0 +1,49 @@
+-- create table departments(
+-- dept_no VARCHAR,
+-- dept_name VARCHAR
+-- );
+--select * from departments;
+
+-- create table dept_emp(
+-- emp_no int,
+-- dept_no VARCHAR,
+-- from_date VARCHAR,
+-- to_date VARCHAR
+-- );
+--Select * from dept_emp;
+
+-- create table dept_manager(
+-- dept_no VARCHAR,
+-- emp_no int,
+-- from_date VARCHAR,
+-- to_date VARCHAR
+-- );
+--Select * from dept_manager;
+
+-- create table employees(
+-- emp_no int,
+-- birth_date VARCHAR,
+-- first_name VARCHAR,
+-- last_name VARCHAR,
+-- gender VARCHAR(2),
+-- hire_date VARCHAR
+-- );
+--select * from employees;
+
+-- create table salaries(
+-- emp_no int,
+-- salary int,
+-- from_date VARCHAR,
+-- to_date VARCHAR
+-- );
+--select * from salaries;
+
+-- create table titles(
+-- emp_no int,
+-- title VARCHAR,
+-- from_data VARCHAR,
+-- to_data VARCHAR
+-- );
+--select * from titles;
+
+
